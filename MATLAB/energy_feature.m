00d9b7b3230b5ac6be3dd8f93c4e0a5054403560
Yue Geng <89446785+YOHNGGG@users.noreply.github.com>
2023-04-05 16:52:03 +0800

    Update energy_feature.m

--- MATLAB/energy_feature.m
+++ MATLAB/energy_feature.m
@@ -1,9 +1,9 @@
 function [energy_feature_0,energy_feature_1] = energy_feature(signal,SNR)
 
-% SNR = -5;
+% Function for generating energy feature of received signal
+
 energy_feature_1 = [];
 energy_feature_0 = [];
-% [t,signal] = QPSK_3(400);
 l = length(signal);
 signal_sample = [];
 for i =1:1:1280
@@ -16,16 +16,6 @@ y_1 = signal_sample;
 y_1 = awgn(y_1,SNR);
 y_0 = y_1 - signal_sample;
 
-% y_1 = normalize(y_1);
-% y_0 = normalize(y_0);
-
-% figure(1)
-% plot(y_1)
-% figure(2)
-% plot(y_0)
-
-% Signal 1
-% energy feature
 Nsample = length(y_1) / 64;
 for n = 1: 1 : 64
     k = 1 + (n-1)*Nsample;
@@ -37,7 +27,6 @@ for n = 1: 1 : 64
     energy_feature_1 = [energy_feature_1 S_sample_energy_1];     
 end
 
-% Signal 0
 Nsample = length(y_0) / 64;
 for n = 1: 1 : 64
     k = 1 + (n-1)*Nsample;
@@ -53,16 +42,3 @@ energy_feature_0 = energy_feature_0';
 energy_feature_1 = energy_feature_1';
 
 end
-
-
-
-%feature matrix
-% feature_1 = [energy_feature_1;cs_feature_1]';
-% feature_0 = [energy_feature_0;cs_feature_0]';
-% 
-% feature = feature_1;
-% 
-% filename = strcat('Signal_1_',num2str(j),'.mat');
-% save(filename, 'feature');
-
-% end
